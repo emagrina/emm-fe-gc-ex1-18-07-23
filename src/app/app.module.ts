62e3f1fb2ea40bd5c54ce6209d35f766650efd77
Ernest Magriñá <ernest8081@gmail.com>
2023-07-18 19:27:11 +0200

    feat: :package: Add pipe capitalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModulesModule } from './modules';
-import { UtilsModule } from './utils';
 
 @NgModule({
   declarations: [
--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -17,6 +17,7 @@ import { FormsModule } from '@angular/forms';
   ],
   exports: [
     NavBarComponent,
+    CapitalizePipe
   ]
 
 })
